05cbc3e2966ada76c9c912733ea8b49f8b5c2e85
jabaraster <jabaraster@gmail.com>
2012-12-17 06:48:54 +0900

    mainte.

---
+++ src/main/java/jabara/sample/web/DumpFilter.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package jabara.sample.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author jabaraster
+ */
+@WebFilter(urlPatterns = { "/*" })
+public class DumpFilter implements Filter {
+
+    @Override
+    public void destroy() {
+        //
+    }
+
+    @Override
+    public void doFilter(final ServletRequest pRequest, final ServletResponse pResponse, final FilterChain pChain) throws IOException,
+            ServletException {
+        final HttpServletRequest request = (HttpServletRequest) pRequest;
+
+        System.out.println(request.getRequestURI());
+        final Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (final Cookie cookie : cookies) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue()); //$NON-NLS-1$
+            }
+        }
+
+        pChain.doFilter(pRequest, pResponse);
+    }
+
+    @Override
+    public void init(@SuppressWarnings("unused") final FilterConfig pFilterConfig) {
+        //
+    }
+
+}
